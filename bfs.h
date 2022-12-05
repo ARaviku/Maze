274688ff0855150fe20d3b42e4373eb764a85051
Anurekha Ravikumar <100729318+ARaviku@users.noreply.github.com>
2022-12-05 11:34:02 -0500

    Update bfs.h

--- bfs.h
+++ bfs.h
@@ -1,31 +1,17 @@
 #pragma once
 #include "maze.h"
-// other includes
-// --- Your code here
 #include<iostream>
 #include<queue>
 
-
-
-// ---
-
-/** \brief Breadth First Search solver */
 class BFS : public TreeSearch {
    public:
     BFS();
-
-    /** \brief Solve the planning problem. Override TreeSearch::solve() */
     bool solve();
 
-    /** \brief add node to the tree */
     bool addNode(const NodePtr& node);
 
    private:
-    // decide what data structure you need
-    // --- Your code here
-    // queue
     std::queue<NodePtr>q_list;
-    
-    // ---
+
 };
 using BFSPtr = std::shared_ptr<BFS>;
